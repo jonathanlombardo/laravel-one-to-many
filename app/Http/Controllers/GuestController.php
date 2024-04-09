7863a537ac5c16d4d8e01f17d6a805b37b33b5a3
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-04-09 13:01:54 +0200

    add query filter into GuestController

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -7,9 +7,20 @@ use Illuminate\Http\Request;
 
 class GuestController extends Controller
 {
-  public function index()
+  public function index($filter_users = null, $filter_types = null, $filter_technologies = null)
   {
-    $projects = Project::orderBy('id', 'desc')->paginate(12);
+    $projects = Project::select();
+
+    if ($filter_users)
+      $projects->whereBelongsTo($filter_users);
+
+    if ($filter_types)
+      $projects->whereBelongsTo($filter_types);
+
+    if ($filter_technologies)
+      $projects->whereBelongsTo($filter_technologies);
+
+    $projects = $projects->orderBy('id', 'desc')->paginate(12);
     return view('guest.index', compact('projects'));
 
   }
