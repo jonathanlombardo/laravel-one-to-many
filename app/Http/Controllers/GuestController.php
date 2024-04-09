af676e29228ee3c999021c8e829647517b9cfa9f
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-04-09 17:50:16 +0200

    feat guest home filters

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -3,25 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class GuestController extends Controller
 {
-  public function index($filter_users = null, $filter_types = null, $filter_technologies = null)
+  public function index(Request $request)
   {
-    $projects = Project::select();
+    $users = User::all();
+    $types = Type::all();
+    $techs = Technology::all();
 
-    if ($filter_users)
-      $projects->whereBelongsTo($filter_users);
+    $numFilter = count($request->all());
 
-    if ($filter_types)
-      $projects->whereBelongsTo($filter_types);
+    $filter_users = $request->input('filter_users') ?? null;
+    $filter_types = $request->input('filter_types') ?? null;
+    $filter_technologies = $request->input('filter_technologies') ?? null;
+
+    $projects = Project::select('projects.*')
+      ->join('project_technology', 'projects.id', '=', 'project_technology.project_id')
+      ->join('technologies', 'technologies.id', '=', 'project_technology.technology_id');
 
     if ($filter_technologies)
-      $projects->whereBelongsTo($filter_technologies);
+      $projects->whereIn('technologies.id', $filter_technologies);
 
-    $projects = $projects->orderBy('id', 'desc')->paginate(12);
-    return view('guest.index', compact('projects'));
+    if ($filter_users)
+      $projects->whereIn('user_id', $filter_users);
+
+    if ($filter_types)
+      $projects->whereIn('type_id', $filter_types);
+
+
+    $projects = $projects->orderBy('id', 'desc')->distinct('projects.id')->paginate(12);
+    return view('guest.index', compact('projects', 'users', 'types', 'techs', 'numFilter'));
+
+  }
+
+  public function filterIndex(Request $request)
+  {
 
   }
 }
--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -14,8 +14,12 @@
 
 @section('maincontent')
   <div class="container">
-    <h1 class="text-center mt-5">HOME</h1>
-    <p class="text-center">All projects</p>
+    <a class="btn btn-outline-primary w-100 my-3" data-bs-toggle="offcanvas" href="#filters" role="button" aria-controls="filters">
+      Filter @if ($numFilter)
+        ({{ $numFilter }})
+      @endif
+    </a>
+    <h1 class="text-center ">HOME</h1>
     <div class="row g-3 py-5">
       @forelse ($projects as $project)
         <div class="col-4">
@@ -40,6 +44,52 @@
       @empty
         <p class="text-center">No projects yet</p>
       @endforelse
+      {{ $projects->links() }}
     </div>
+  </div>
+@endsection
+
+@section('modals')
+  <div class="offcanvas offcanvas-start" tabindex="-1" id="filters" aria-labelledby="filtersLabel">
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="filtersLabel">Offcanvas</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <form method="GET" class="offcanvas-body">
+      <div class="fw-bold">
+        Choose the filters and press "Filter" button
+      </div>
+      @if ($users)
+        <div class="mt-4 mb-2">Author</div>
+        @foreach ($users as $user)
+          <div class="mb-2">
+            <input {{ in_array($user->id, request('filter_users', [])) ? 'checked' : '' }} type="checkbox" class="form-check-input" name="filter_users[]" id="filter-users-{{ $user->id }}" value="{{ $user->id }}">
+            <label for="filter-users-{{ $user->id }}">{{ $user->name }}</label>
+          </div>
+        @endforeach
+      @endif
+      @if ($types)
+        <div class="mt-4 mb-2">Type</div>
+        @foreach ($types as $type)
+          <div class="mb-2">
+            <input {{ in_array($type->id, request('filter_types', [])) ? 'checked' : '' }} type="checkbox" class="form-check-input" name="filter_types[]" id="filter-types-{{ $type->id }}" value="{{ $type->id }}">
+            <label for="filter-types-{{ $type->id }}">{{ $type->label }}</label>
+          </div>
+        @endforeach
+      @endif
+      @if ($techs)
+        <div class="mt-4 mb-2">Type</div>
+        @foreach ($techs as $tech)
+          <div class="mb-2">
+            <input {{ in_array($tech->id, request('filter_technologies', [])) ? 'checked' : '' }} type="checkbox" class="form-check-input" name="filter_technologies[]" id="filter-technologies-{{ $tech->id }}" value="{{ $tech->id }}">
+            <label for="filter-technologies-{{ $tech->id }}">{{ $tech->label }}</label>
+          </div>
+        @endforeach
+      @endif
+
+      <button class="btn btn-success w-100 mt-3">Filter</button>
+      <a href="{{ route('guest.index') }}" class="btn btn-outline-secondary w-100 mt-3">Reset Filter</a>
+
+    </form>
   </div>
 @endsection
