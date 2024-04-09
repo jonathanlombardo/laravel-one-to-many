b6024cd2395dc471732258c3ad959af040159091
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-04-09 12:25:35 +0200

    organize route

--- routes/web.php
+++ routes/web.php
@@ -18,50 +18,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// #GUEST
+Route::name('guest.')->group(function () {
+  Route::get('/', [GuestController::class, 'index'])->name('index');
+});
 
-Route::controller(GuestController::class)
-  ->name('guest.')
-  ->group(function () {
-    Route::get('/', 'index')->name('index');
-  });
+// #AUTH
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
+  // GENERAL
+  Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
 
-Route::middleware('auth')
-  ->prefix('admin')
-  ->name('admin.')
-  ->group(function () {
-    Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+  // PROJECTS
+  Route::resource('projects', ProjectController::class);
 
-    // PROJECTS ROUTES
-    Route::resource('projects', ProjectController::class);
+  // TYPES
+  Route::resource('types', TypeController::class)->middleware('role.admin')->except(['index', 'show']);
+  Route::resource('types', TypeController::class)->only(['index', 'show']);
 
-    // TYPES ROUTES
-    Route::middleware('role.admin')->group(function () {
-      Route::post('/types', [TypeController::class, 'store'])->name('types.store');
-      Route::get('/types/create', [TypeController::class, 'create'])->name('types.create');
-      Route::patch('/types/{type}', [TypeController::class, 'update'])->name('types.update');
-      Route::delete('/types/{type}', [TypeController::class, 'destroy'])->name('types.destroy');
-      Route::get('/types/{type}/edit', [TypeController::class, 'edit'])->name('types.edit');
-    });
-    Route::get('/types', [TypeController::class, 'index'])->name('types.index');
-    Route::get('/types/{type}', [TypeController::class, 'show'])->name('types.show');
-
-    // TECHNOLOGIES ROUTES
-    Route::middleware('role.admin')->group(function () {
-      Route::post('/technologies', [TechnologyController::class, 'store'])->name('technologies.store');
-      Route::get('/technologies/create', [TechnologyController::class, 'create'])->name('technologies.create');
-      Route::patch('/technologies/{technology}', [TechnologyController::class, 'update'])->name('technologies.update');
-      Route::delete('/technologies/{technology}', [TechnologyController::class, 'destroy'])->name('technologies.destroy');
-      Route::get('/technologies/{technology}/edit', [TechnologyController::class, 'edit'])->name('technologies.edit');
-    });
-    Route::get('/technologies', [TechnologyController::class, 'index'])->name('technologies.index');
-    Route::get('/technologies/{technology}', [TechnologyController::class, 'show'])->name('technologies.show');
-
-
-  });
-
-
+  // TECHNOLOGIES
+  Route::resource('technologies', TypeController::class)->middleware('role.admin')->except(['index', 'show']);
+  Route::resource('technologies', TypeController::class)->only(['index', 'show']);
+});
 
 require __DIR__ . '/auth.php';
-
-// GET|HEAD        admin/types/create .......................................................... admin.types.create › Admin\TypeController@create  
-// GET|HEAD        admin/types/create .......................................................... admin.types.create › Admin\TypeController@create  
