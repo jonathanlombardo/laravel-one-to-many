253fa8ea0e12ea842ab4c5ebd034b5351813a1b6
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-04-11 18:21:16 +0200

    fix: project form

--- resources/views/admin/projects/form.blade.php
+++ resources/views/admin/projects/form.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.main')
 
+@section('assets')
+  <style>
+    .proj-img {
+      width: 100%;
+      height: 270px;
+      object-fit: cover;
+      object-position: top;
+    }
+  </style>
+@endsection
+
 @section('maincontent')
   <div class="container my-5">
-    <h1>{{ $editForm ? 'Edit project' : 'Create Project' }}</h1>
+    <h1>{{ $project->id ? 'Edit project' : 'Create Project' }}</h1>
 
     @if ($errors->any())
       <div class="error-alert alert alert-danger alert-dismissible fade show" role="alert">
@@ -11,28 +22,21 @@
       </div>
     @endif
 
-    <form enctype="multipart/form-data" action="{{ $editForm ? route('admin.projects.update', $project) : route('admin.projects.store') }}" method="POST" class="row my-4 g-3">
+
+
+    <form enctype="multipart/form-data" action="{{ $project->id ? route('admin.projects.update', $project) : route('admin.projects.store') }}" method="POST" class="row my-4 g-3">
       @csrf
-      @method($editForm ? 'PATCH' : 'POST')
+      @method($project->id ? 'PATCH' : 'POST')
 
       <div class="col-6">
         <div class="form-floating">
-          <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" placeholder="title" value="{{ $editForm ? ($errors->any() ? old('title') : $project->title) : old('title') }}">
+          <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" placeholder="title" value="{{ $project->id ? ($errors->any() ? old('title') : $project->title) : old('title') }}">
           <label for="title">Project Title</label>
           @error('title')
             <div class="invalid-feedback">{{ $message }}</div>
           @enderror
         </div>
       </div>
-      {{-- <div class="col-4">
-        <div class="form-floating">
-          <input type="text" class="form-control @error('author') is-invalid @enderror" id="author" name="author" placeholder="author" value="{{ $editForm ? ($errors->any() ? old('author') : $project->author) : old('author') }}">
-          <label for="author">Project Author</label>
-          @error('author')
-            <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
-        </div>
-      </div> --}}
       <div class="col-6">
         <div class="form-floating">
 
@@ -40,7 +44,7 @@
             <select class="form-select @error('type_id') is-invalid @enderror" id="type_id" name="type_id" aria-label="Type select">
               <option value="" class="d-none">Select a project type</option>
               @foreach ($types as $type)
-                <option value="{{ $type->id }}" {{ $type->id == old('type_id', $editForm ? $project->type->id : '') ? 'selected' : '' }}>
+                <option value="{{ $type->id }}" {{ $type->id == old('type_id', $project->id ? $project->type->id : '') ? 'selected' : '' }}>
                   {{ $type->label }}
                 </option>
               @endforeach
@@ -57,7 +61,7 @@
         <div class="row g-3">
           @foreach ($technologies as $techology)
             <div class="col-2 @error('techs') is-invalid @enderror">
-              <input {{ in_array($techology->id, old('techs', $editForm ? $projTechIds : [])) ? 'checked' : '' }} id="tech-{{ $techology->id }}" name="techs[]" value="{{ $techology->id }}" type="checkbox" class="form-check-input @error('techs') is-invalid @enderror">
+              <input {{ in_array($techology->id, old('techs', $project->id ? $projTechIds : [])) ? 'checked' : '' }} id="tech-{{ $techology->id }}" name="techs[]" value="{{ $techology->id }}" type="checkbox" class="form-check-input @error('techs') is-invalid @enderror">
               <label class="form-check-label" for="tech-{{ $techology->id }}">{{ $techology->label }}</label>
             </div>
           @endforeach
@@ -68,27 +72,33 @@
         <hr>
 
       </div>
-      <div class="col-6">
+      <div class="col-12">
         <div class="form-floating">
-          <input type="url" class="form-control @error('git_hub') is-invalid @enderror" id="git_hub" name="git_hub" placeholder="git_hub" value="{{ $editForm ? ($errors->any() ? old('git_hub') : $project->git_hub) : old('git_hub') }}">
+          <input type="url" class="form-control @error('git_hub') is-invalid @enderror" id="git_hub" name="git_hub" placeholder="git_hub" value="{{ $project->id ? ($errors->any() ? old('git_hub') : $project->git_hub) : old('git_hub') }}">
           <label for="git_hub">GitHub Repo URL</label>
           @error('git_hub')
             <div class="invalid-feedback">{{ $message }}</div>
           @enderror
         </div>
       </div>
-      <div class="col-6">
+      <div class="col-8">
         <div class="form-floating">
-          <input type="file" class="form-control @error('image') is-invalid @enderror" id="image" name="image" placeholder="image" value="{{ $editForm ? ($errors->any() ? old('image') : $project->image) : old('image') }}">
+          <input type="file" class="proj-img-input form-control @error('image') is-invalid @enderror" id="image" name="image" placeholder="image" value="{{ $project->id ? ($errors->any() ? old('image') : $project->image) : old('image') }}">
           <label for="image">Image</label>
-          @error('image')
-            <div class="invalid-feedback">{{ $message }}</div>
-          @enderror
+          <div class="invalid-feedback img-fb">
+            @error('image')
+              {{ $message }}
+            @enderror
+          </div>
         </div>
       </div>
+      <div class="col-4">
+        <img src="{{ $project->getImgUrl() }}" alt="" class="proj-img">
+      </div>
+
       <div class="col-12">
         <div class="form-floating">
-          <textarea class="form-control @error('description') is-invalid @enderror" placeholder="description" id="description" name="description" style="height: 100px">{{ $editForm ? ($errors->any() ? old('description') : $project->description) : old('description') }}</textarea>
+          <textarea class="form-control @error('description') is-invalid @enderror" placeholder="description" id="description" name="description" style="height: 100px">{{ $project->id ? ($errors->any() ? old('description') : $project->description) : old('description') }}</textarea>
           <label for="description">Project Description</label>
           @error('description')
             <div class="invalid-feedback">{{ $message }}</div>
@@ -96,7 +106,7 @@
         </div>
       </div>
       <div class="col-12">
-        <button class="btn btn-success">{{ $editForm ? 'Edit project' : 'Save' }}</button>
+        <button class="btn btn-success">{{ $project->id ? 'Edit project' : 'Save' }}</button>
         <a href="{{ route('admin.projects.index') }}" class="btn btn-warning">Back to projects list</a>
       </div>
 
@@ -115,27 +125,55 @@
     inputs.forEach((input) => {
       input.addEventListener("input", function() {
         this.classList.remove('is-invalid')
-        errorAlertEl.classList.add('d-none')
+        if (errorAlertEl) errorAlertEl.classList.add('d-none')
       })
 
       input.addEventListener("change", function() {
         this.classList.remove('is-invalid')
-        errorAlertEl.classList.add('d-none')
+        if (errorAlertEl) errorAlertEl.classList.add('d-none')
       })
     });
 
     textareas.forEach((textarea) => {
       textarea.addEventListener("change", function() {
         this.classList.remove('is-invalid')
-        errorAlertEl.classList.add('d-none')
+        if (errorAlertEl) errorAlertEl.classList.add('d-none')
       })
     });
 
     selects.forEach((select) => {
       select.addEventListener("change", function() {
         this.classList.remove('is-invalid')
-        errorAlertEl.classList.add('d-none')
+        if (errorAlertEl) errorAlertEl.classList.add('d-none')
       })
     });
+
+    const projImg = document.querySelector('.proj-img');
+    const projImgInput = document.querySelector('.proj-img-input');
+    const fbImgEl = document.querySelector('.img-fb');
+    const originalSrc = projImg.src;
+
+    projImgInput.addEventListener("change", function() {
+      const file = this.files[0];
+
+      if (file.type.startsWith('image')) {
+        const reader = new FileReader();
+        console.log(this.files[0].type);
+        reader.onloadend = function() {
+          projImg.src = reader.result;
+        }
+        if (file) {
+          reader.readAsDataURL(file);
+        } else {
+          projImg.src = originalSrc;
+        }
+      } else {
+        projImg.src = originalSrc
+        projImgInput.value = null;
+        projImgInput.classList.add('is-invalid')
+        fbImgEl.innerText = 'The image must be an image'
+      }
+
+    })
   </script>
 @endsection
